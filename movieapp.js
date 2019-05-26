121790783d87ede111e0a57d36a5306f613e5fdc
agosgal <agosgal@gmail.com>
2019-05-26 20:02:14 -0300

    arreglo_loadmore

--- movieapp.js
+++ movieapp.js
@@ -1,14 +1,23 @@
-const logo = document.querySelector("#header .logo");
+// Vuelve a la home al hacer click
 
-console.log(logo)
+const logo = document.querySelector("#header .logo");
 
 logo.onclick = function (event) {
  location.reload(true);
 }
 
-// Traigo las 5 populares
+
+// Ni bien abro el sitio
 
 const traerpeli = (categoria) => {
+
+    if (categoria === "top_rated") {
+        const divboton = document.querySelector("button");
+        divboton.classList.add("hide");
+    }
+    
+    // Traigo las 5 populares
+    
     const prom = fetch("https://api.themoviedb.org/3/movie/" + categoria + "?api_key=d970e377bc07fda41fabc4c1bc412609");
     prom.then(res => res.json())
         .then(movie => {    
@@ -30,7 +39,6 @@ const traerpeli = (categoria) => {
                 div.appendChild(titulo)
 
                 const lista = document.getElementById("listadepelis" + categoria)
-                console.log(lista)
 
                 // Agregamos el div al listado 
                 lista.appendChild(div)
@@ -62,21 +70,28 @@ const banner = document.querySelector("#contenido .banner");
 const textotop = document.querySelector(".top-rated .categoria .textotop")
 const textoview = document.querySelector(".top-rated .categoria .all");
 const listadepelis = document.getElementById("listadepelistop_rated");
-
-console.log(viewPop)
+const divboton = document.querySelector(".top-rated .opciones .divboton")
 
 // Armo funcion para traer todos los resultados de la categoría
 
 // POPULAR
 
 
-const traigopopular = () => {
+const traigopeli = (categoria) => {
+    const divboton = document.querySelector("button");
+    divboton.classList.remove("hide");
+    banner.classList.add("hide");
+    listaPop.classList.add("hide");
+    listaUp.classList.add("hide");
+    listaNow.classList.add("hide");
+    listaTop.classList.add("show");
     let vuelta = 1;
     const listadepelis = document.getElementById("listadepelistop_rated");
-    const prom = fetch("https://api.themoviedb.org/3/movie/popular?api_key=d970e377bc07fda41fabc4c1bc412609&page=" + vuelta);
+    const prom = fetch("https://api.themoviedb.org/3/movie/" + categoria + "?api_key=d970e377bc07fda41fabc4c1bc412609&page=" + vuelta);
     prom.then(res => res.json())
         .then(movie => {    
             if (movie) {
+                listadepelis.innerHTML = "";
             for (let i = 0; i < movie.results.length ; i++) {
                 // Creamos un div, le añadimos el nombre y la imagen 
                 const div = document.createElement("div")
@@ -89,8 +104,6 @@ const traigopopular = () => {
                 const imagen = document.createElement("img");
                 imagen.setAttribute('src', "https://image.tmdb.org/t/p/w370_and_h556_bestv2" + movie.results[i].poster_path);
 
-                const boton = document.createElement("button");
-
                 div.appendChild(imagen)
                 div.appendChild(titulo);
                 
@@ -100,16 +113,13 @@ const traigopopular = () => {
                 listadepelis.appendChild(div);
 
                 }
-                const boton = document.createElement("div");
-                boton.innerHTML = "LOAD MORE";
-                boton.classList.add("boton");
-                listadepelis.appendChild(boton);
 
-                boton.onclick = function (event) {
-                    boton.classList.add("hide");
+                // Si presiono el botón
+
+                divboton.onclick = function (event) {
                     vuelta = (vuelta + 1);
                     const listadepelis = document.getElementById("listadepelistop_rated");
-                    const prom = fetch("https://api.themoviedb.org/3/movie/popular?api_key=d970e377bc07fda41fabc4c1bc412609&page=" + vuelta);
+                    const prom = fetch("https://api.themoviedb.org/3/movie/" + categoria + "?api_key=d970e377bc07fda41fabc4c1bc412609&page=" + vuelta);
                     prom.then(res => res.json())
                         .then(movie => {    
                             if (movie) {
@@ -136,10 +146,6 @@ const traigopopular = () => {
                                 listadepelis.appendChild(div);
                 
                                 }
-                                const boton = document.createElement("div");
-                                boton.innerHTML = "LOAD MORE";
-                                boton.classList.add("boton");
-                                listadepelis.appendChild(boton);
                 
                             }
                 })
@@ -154,245 +160,49 @@ const traigopopular = () => {
 
 
 iconoPopular.onclick = function (event) {
-    banner.classList.add("hide");
-    listaPop.classList.add("hide");
-    listaUp.classList.add("hide");
-    listaNow.classList.add("hide");
-    listaTop.classList.add("show");
+    textoview.innerHTML = "X Results";
     textotop.innerHTML = "Popular Movies";
-    textoview.innerHTML = "2000 results";
-    listadepelis.innerHTML = "";
-    traigopopular();
-}
+    traigopeli("popular");
+}   
 
 viewPop.onclick = function (event) {
-    banner.classList.add("hide");
-    listaPop.classList.add("hide");
-    listaUp.classList.add("hide");
-    listaNow.classList.add("hide");
-    listaTop.classList.add("show");
+    textoview.innerHTML = "X Results";
     textotop.innerHTML = "Popular Movies";
-    textoview.innerHTML = "2000 results";
-    listadepelis.innerHTML = "";
-    traigopopular()
+    traigopeli("popular");
 }
 
-// // TOP
+iconoTop.onclick = function (event) {
+    textoview.innerHTML = "X Results";
+    textotop.innerHTML = "Top Rated Movies";
+    traigopeli("top_rated");
+}   
 
-// const traigotop = () => {
-//     const listadepelis = document.getElementById("listadepelistop_rated");
-//     const prom = fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d970e377bc07fda41fabc4c1bc412609");
-//     prom.then(res => res.json())
-//         .then(movie => {    
-//             const resultados = movie.results.length;
-//             if (movie) {
-//             for (let i = 0; i < movie.results.length; i++) {
-//                 // Creamos un div, le añadimos el nombre y la imagen 
-//                 const div = document.createElement("div")
-//                 div.classList.add("movie")
+viewTop.onclick = function (event) {
+    textoview.innerHTML = "X Results";
+    textotop.innerHTML = "Top Rated Movies";
+    traigopeli("top_rated");
+}
 
-//                 const titulo = document.createElement("p")
-//                 titulo.classList.add("titulo")
-//                 titulo.textContent = movie.results[i].title
+iconoUp.onclick = function (event) {
+    textoview.innerHTML = "X Results";
+    textotop.innerHTML = "Upcomung Movies";
+    traigopeli("upcoming");
+}   
 
-//                 const imagen = document.createElement("img");
-//                 imagen.setAttribute('src', "https://image.tmdb.org/t/p/w370_and_h556_bestv2" + movie.results[i].poster_path);
+viewUp.onclick = function (event) {
+    textoview.innerHTML = "X Results";
+    textotop.innerHTML = "Upcomung Movies";
+    traigopeli("upcoming");
+}
 
-//                 div.appendChild(imagen)
-//                 div.appendChild(titulo)
+iconoNow.onclick = function (event) {
+    textoview.innerHTML = "X Results";
+    textotop.innerHTML = "Now Playing Movies";
+    traigopeli("now_playing");
+}   
 
-//                 // Agregamos el div al listado 
-//                 listadepelis.appendChild(div)
-//             }
-//         }
-//     })
-// }
-// // Cuando clickeo en cada categoría del costado
-
-
-// iconoTop.onclick = function (event) {
-//     banner.classList.add("hide");
-//     listaPop.classList.add("hide");
-//     listaUp.classList.add("hide");
-//     listaNow.classList.add("hide");
-//     listaTop.classList.add("show");
-//     textotop.innerHTML = "Top Rated Movies";
-//     textoview.innerHTML = "2000 results";
-//     listadepelis.innerHTML = "";
-//     traigotop()
-// }
-
-// viewTop.onclick = function (event) {
-//     banner.classList.add("hide");
-//     listaPop.classList.add("hide");
-//     listaUp.classList.add("hide");
-//     listaNow.classList.add("hide");
-//     listaTop.classList.add("show");
-//     textotop.innerHTML = "Top Rated Movies";
-//     textoview.innerHTML = "2000 results";
-//     listadepelis.innerHTML = "";
-//     traigotop()
-// }
-
-// // UPCOMING
-
-// const traigoup = () => {
-//     const listadepelis = document.getElementById("listadepelistop_rated");
-//     const prom = fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d970e377bc07fda41fabc4c1bc412609");
-//     prom.then(res => res.json())
-//         .then(movie => {    
-//             const resultados = movie.results.length;
-//             if (movie) {
-//             for (let i = 0; i < movie.results.length; i++) {
-//                 // Creamos un div, le añadimos el nombre y la imagen 
-//                 const div = document.createElement("div")
-//                 div.classList.add("movie")
-
-//                 const titulo = document.createElement("p")
-//                 titulo.classList.add("titulo")
-//                 titulo.textContent = movie.results[i].title
-
-//                 const imagen = document.createElement("img");
-//                 imagen.setAttribute('src', "https://image.tmdb.org/t/p/w370_and_h556_bestv2" + movie.results[i].poster_path);
-
-//                 div.appendChild(imagen)
-//                 div.appendChild(titulo)
-
-//                 // Agregamos el div al listado 
-//                 listadepelis.appendChild(div)
-//             }
-//         }
-//     })
-// }
-// // Cuando clickeo en cada categoría del costado
-
-
-// iconoUp.onclick = function (event) {
-//     banner.classList.add("hide");
-//     listaPop.classList.add("hide");
-//     listaUp.classList.add("hide");
-//     listaNow.classList.add("hide");
-//     listaTop.classList.add("show");
-//     textotop.innerHTML = "Upcoming Movies";
-//     textoview.innerHTML = "2000 results";
-//     listadepelis.innerHTML = "";
-//     traigoup()
-// }
-
-// viewUp.onclick = function (event) {
-//     banner.classList.add("hide");
-//     listaPop.classList.add("hide");
-//     listaUp.classList.add("hide");
-//     listaNow.classList.add("hide");
-//     listaTop.classList.add("show");
-//     textotop.innerHTML = "Upcoming Movies";
-//     textoview.innerHTML = "2000 results";
-//     listadepelis.innerHTML = "";
-//     traigoup()
-// }
-
-// // NOW PLAYING
-
-// const traigonow = () => {
-//     const listadepelis = document.getElementById("listadepelistop_rated");
-//     const prom = fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=d970e377bc07fda41fabc4c1bc412609");
-//     prom.then(res => res.json())
-//         .then(movie => {    
-//             const resultados = movie.results.length;
-//             if (movie) {
-//             for (let i = 0; i < movie.results.length; i++) {
-//                 // Creamos un div, le añadimos el nombre y la imagen 
-//                 const div = document.createElement("div")
-//                 div.classList.add("movie")
-
-//                 const titulo = document.createElement("p")
-//                 titulo.classList.add("titulo")
-//                 titulo.textContent = movie.results[i].title
-
-//                 const imagen = document.createElement("img");
-//                 imagen.setAttribute('src', "https://image.tmdb.org/t/p/w370_and_h556_bestv2" + movie.results[i].poster_path);
-
-//                 div.appendChild(imagen)
-//                 div.appendChild(titulo)
-
-//                 // Agregamos el div al listado 
-//                 listadepelis.appendChild(div)
-//             }
-//         }
-//     })
-// }
-// // Cuando clickeo en cada categoría del costado
-
-
-// iconoNow.onclick = function (event) {
-//     banner.classList.add("hide");
-//     listaPop.classList.add("hide");
-//     listaUp.classList.add("hide");
-//     listaNow.classList.add("hide");
-//     listaTop.classList.add("show");
-//     textotop.innerHTML = "Now Playing Movies";
-//     textoview.innerHTML = "2000 results";
-//     listadepelis.innerHTML = "";
-//     traigonow()
-// }
-
-// viewNow.onclick = function (event) {
-//     banner.classList.add("hide");
-//     listaPop.classList.add("hide");
-//     listaUp.classList.add("hide");
-//     listaNow.classList.add("hide");
-//     listaTop.classList.add("show");
-//     textotop.innerHTML = "Now Playing Movies";
-//     textoview.innerHTML = "2000 results";
-//     listadepelis.innerHTML = "";
-//     traigonow()
-// }
-
-// // Resultados con buscador
-
-// const buscador = document.querySelector("#header .buscador1 .buscador");
-
-// console.log(buscador)
-
-// buscador.addEventListener('keypress', function (event) {
-//     if (event.keyCode === 13) {
-//         banner.classList.add("hide");
-//         listaPop.classList.add("hide");
-//         listaUp.classList.add("hide");
-//         listaNow.classList.add("hide");
-//         listaTop.classList.add("show");
-//         textotop.innerHTML = "Results for: " + buscador.value;
-//         textoview.innerHTML = "2000 results";
-//         listadepelis.innerHTML = "";
-//         buscoPeli()
-//   }
-// })
-
-// const buscoPeli = () => {
-//     const elegida = buscador.value;
-//     const prom = fetch("https://api.themoviedb.org/3/search/movie?api_key=d970e377bc07fda41fabc4c1bc412609&query=" + elegida);
-//     prom.then(res => res.json())
-//         .then(movie => {    
-//             const resultados = movie.results.length;
-//             if (movie) {
-//             for (let i = 0; i < movie.results.length; i++) {
-//                 // Creamos un div, le añadimos el nombre y la imagen 
-//                 const div = document.createElement("div")
-//                 div.classList.add("movie")
-
-//                 const titulo = document.createElement("p")
-//                 titulo.classList.add("titulo")
-//                 titulo.textContent = movie.results[i].title
-
-//                 const imagen = document.createElement("img");
-//                 imagen.setAttribute('src', "https://image.tmdb.org/t/p/w370_and_h556_bestv2" + movie.results[i].poster_path);
-
-//                 div.appendChild(imagen)
-//                 div.appendChild(titulo)
-
-//                 // Agregamos el div al listado 
-//                 listadepelis.appendChild(div)
-//             }
-//         }
-//     })
-// }
+viewNow.onclick = function (event) {
+    textoview.innerHTML = "X Results";
+    textotop.innerHTML = "Now Playing Movies";
+    traigopeli("now_playing");
+}
